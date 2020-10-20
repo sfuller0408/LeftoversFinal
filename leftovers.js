dd34337b8646f741e5607da075b10a8f0b9beb63
Shawn Fuller <sfuller4@my.tnstate.edu>
2020-10-20 16:51:11 +0000

    Created Base homepage with title and searchbar. No functionality as of yet. Code reviewed by HTate.

--- leftovers.js
+++ leftovers.js
@@ -5,10 +5,18 @@ if (port == null || port == "") {
     port = 80;
 }
 
+function home(req, res) {
+    res.render('search');
+}
+
 // Set up the handlers for Node.js
 app.use(express.static("static"));      // static files live in "static" folder
+app.set('views', './views');
+app.set('view engine', 'pug');
 
-//app.get();
+app.get('/', (req, res) => {
+    home(req, res);
+});
 
 // Start Express listening at the given port
 app.listen(port, () => {
