96f1606dab89b317190c335989ab4f60ea4d70ea
Brianna Rogers <briannanrogers915@gmail.com>
2020-10-20 00:58:06 +0000

    Created database

--- leftovers.js
+++ leftovers.js
@@ -5,6 +5,23 @@ if (port == null || port == "") {
     port = 80;
 }
 
+// Create database to hold results
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database("./leftovers.db",
+  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+    console.log("Connected to db");
+    }
+  }
+);
+
+// Create tables if it doesn't already exist
+// db.run(
+  // 'CREATE TABLE IF NOT EXISTS ')
+
 // Set up the handlers for Node.js
 app.use(express.static("static"));      // static files live in "static" folder
 
