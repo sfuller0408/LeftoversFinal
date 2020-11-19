0a10b0f11523ae1e2d6001bfb621f9ae142f671a
Shawn Fuller <sfuller4@my.tnstate.edu>
2020-11-19 01:15:17 +0000

    Added gherkins for first P0

---
+++ features/signIn.feature
@@ -0,0 +1,5 @@
+Feature: Access Home/Search Page as guest
+  Scenario: User does not have username and password
+    Given user clicks "sign in as guest" button
+    When loginAsGuest is called
+    Then /search path is returned
---
+++ steps/signInSteps.js
@@ -0,0 +1,22 @@
+const cucumber = require("cucumber");
+const expect = require("chai").expect;
+const Given = cucumber.Given;
+const When = cucumber.When;
+const Then = cucumber.Then;
+const signIn = require("../static/signIn/signIn.js");
+
+Given("user clicks \"sign in as guest\" button",
+        function() {
+    // nothing is done here because a mock button press is not
+    // needed to call the loginAsGuest function.
+});
+
+let result;
+
+When("loginAsGuest is called", function() {
+    result = signIn.loginAsGuest();
+});
+
+Then("/search path is returned", function() {
+    expect(result).to.equal("/search");
+});
